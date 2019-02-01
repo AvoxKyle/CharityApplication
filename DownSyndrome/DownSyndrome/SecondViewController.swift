436886ba29179b0d3d3b4d68b8fd6e7531cf4649
SierraK <S529039@nwmissouri.edu>
2019-02-01 13:53:21 -0600

    basic tabbed skeleton

---
+++ DownSyndrome/DownSyndrome/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DownSyndrome
+//
+//  Created by Sierra on 2/1/19.
+//  Copyright © 2019 Sierra King. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
