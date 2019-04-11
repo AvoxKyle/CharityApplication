2b7707c4c62b89ad92ac2c441e5f0a302e605c7f
SierraK <S529039@nwmissouri.edu>
2019-04-10 19:41:45 -0500

    Fixed SwiftSoup

--- CharityApp/CharityApp/ViewController.swift
+++ CharityApp/CharityApp/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import SwiftSoup
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-  
+        let url = "https://dsaofnwi.org/"
+        
+        guard let myURL = URL(string: url) else {
+            print("Error: \(String(describing: url)) doesn't seem to be a valid URL")
+            return
+        }
+        
+        
+        do {
+            let html = try! String(contentsOf: myURL, encoding: .utf8)
+            //guard let elements: Elements = try? SwiftSoup.parse(html).select("div [class="primary-content"]") else {return}
+        } catch let error {
+            print("Error:", error)
+        }
+        
     }
     
 
